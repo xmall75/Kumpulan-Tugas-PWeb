a51e66e751166b002f9db64b91450a0a38e8908d
Revanantyo D <34641833+xmall75@users.noreply.github.com>
2023-11-07 15:00:24 +0700

    Add files via upload

---
+++ pweb-tugas10/list.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>CRUD Using PHP/MySQL</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="css/plugins/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+
+</head>
+
+
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">CRUD Using PHP/MySQL</a>
+            </div>
+     
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                           PHP CRUD <small><a href="index.php">Mendaftar sebagai siswa baru</a></small>
+                        </h1>
+                       
+                    </div>
+                </div>
+                <!-- /.row -->
+
+
+             <div class="col-lg-12">
+                  <h2>User Information's</h2>
+                      <div class="col-lg-6">
+                       <?php include 'connection.php'; ?>
+
+                        <?php $results = mysqli_query($db, "SELECT * FROM people"); ?>
+
+<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+    <thead>
+        <tr>
+            <th>Nama Lengkap</th>
+            <th>Jenis Kelamin</th>
+            <th>Alamat</th>
+            <th>Email</th>
+            <th>Contact</th>
+            <th>NIK</th>
+        </tr>
+    </thead>
+    
+    <?php while ($row = mysqli_fetch_array($results)) { ?>
+        <tr>
+            <td><?php echo $row['name']; ?></td>
+            <td><?php echo $row['kelamin']; ?></td>
+             <td><?php echo $row['alamat']; ?></td>
+            <td><?php echo $row['email']; ?></td>
+             <td><?php echo $row['contact']; ?></td>
+            <td><?php echo $row['nik']; ?></td>
+        </tr>
+    <?php } ?>
+</table>
+                    </div>
+                </div>
+                
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="js/plugins/morris/raphael.min.js"></script>
+    <script src="js/plugins/morris/morris.min.js"></script>
+    <script src="js/plugins/morris/morris-data.js"></script>
+
+</body>
+</html>
